f5adc897534fe55ef7092a3cf950e6e4fb41bc49
KristaCalleja <kristacm@gmail.com>
2021-02-22 18:58:13 -0300

    Add answer method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
     end
 
     def answer
+        ask = gets.chomp
+
+        if ask == "?"
+            puts "Silly question, get dressed and go to work!"
+        elsif 
+            ask == "I am going to work."
+            puts "Great!"
+        else
+            puts "I don't care, get dressed and go to work!"
+        end
     end
+    
 end
