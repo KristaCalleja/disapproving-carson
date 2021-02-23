af9fde3a70a0249edc1309f994a2c2e67ace4a37
KristaCalleja <kristacm@gmail.com>
2021-02-23 11:31:48 -0300

    Add time question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,14 +5,14 @@ class QuestionsController < ApplicationController
     def answer
         @ask = params[:ask]
 
-        if @ask.include?("?")
-            @answer = "I'm afraid I'm not aware. In any case, I'll be there in the morning, m'lady. Don't you worry."
+        if @ask == ("What's the time?")
+            @answer = "M'Lord, considering your time-zone and the position of the planets and the sun, the hour is now striking #{Time.now.hour}"
         elsif 
-            @ask == "I want to party!"
-            @answer = "This sounds very like the kind of boys' talk that I do not allow!"
+            @ask.include?("?")
+            @answer = "I'm afraid I'm not aware. In any case, I'll be there in the morning, m'lady. Don't you worry."
         else
             @answer = "What a topsy turvy world we've come to!"
         end
-    end
+    end    
 
 end
